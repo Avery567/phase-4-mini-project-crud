30cde016284e0a8dfd39d276d4ad3b060f1a6cef
Avery Lin <tclin100@gmail.com>
2021-11-24 10:24:19 -0500

    commit tree 033f505507ee061e518f6b13ec1347ecec8f36b4

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,37 @@
+class SpicesController < ApplicationController
+    
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+    
+    def create
+        spice = Spice.create(title: params[:title], image:params[:image], description:params[:description], notes: params[:notes], rating:params[:rating] )
+        render json: spice, status: :created
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :note, :rating)
+    end
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        if spice
+            spice.update(spice_params)
+            render json: spice
+        else
+            render json: { error: "Spice not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        if spice
+            spice.destroy
+            head :no_content
+        else
+            render json: { error: "Spice not found" }, status: :not_found
+        end
+    end
+
+end
